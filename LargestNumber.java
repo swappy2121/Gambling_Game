95e81da31f4f84140ab57cdbea5775a83f2c8c18
swapnil <sopiswarrinku77@gmail.com>
2022-04-20 08:41:57 -0700

    Added ; gambling game bet per day

---
+++ LargestNumber.java
@@ -0,0 +1,14 @@
+
+package com.company;
+
+
+public class GamblerGameUC1 {
+
+    public static void main(String[] args) {
+        int Stake = 100;
+        int Bet = 1;
+        System.out.println("the stakeholder should bet per day: " + Stake+"$");
+        System.out.println("the bet for one game :" + Bet+"$");
+    }
+}
+
